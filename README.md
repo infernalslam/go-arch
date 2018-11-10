e385313df6b77b1a2c406fef36d7a7a517d6421d
infernalslam <infernal-slam@hotmail.com>
2018-11-11 03:46:48 +0700

    feature: init

---
+++ README.md
@@ -0,0 +1,10 @@
+golang-viper-config-example
+Simple example. Currently no tests implemented, no validity check of given parameters. Feel free to add tickets for more details, documentation, other examples
+
+To run this example in linux use following:
+
+```
+go get github.com/devilsray/golang-viper-config-example
+cd $GOPATH/src/github.com/devilsray/golang-viper-config-example/
+go run *.go
+```
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)", dbUser, dbPass, dbHost, dbPort)
+	fmt.Println(connection)
+
+	fmt.Println("tak")
+}
